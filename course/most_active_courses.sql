3b934eee8e4140be3ea6a0e761a391d11ac4ae4a
tenkaikami <edward.lin.ait@gmail.com>
2016-11-24 21:40:27 +0800

    Update most_active_courses.sql

--- course/most_active_courses.sql
+++ course/most_active_courses.sql
@@ -1,6 +1,6 @@
 Most active courses ###
 Most active courses ###
-SELECT COUNT(l.id) hits, l.course courseId, c.fullname coursename
-FROM prefix_log l INNER JOIN prefix_course c ON l.course = c.id
-GROUP BY courseId
-ORDER BY hits DESC
+SELECT COUNT( l.id ) hits, l.courseid courseId, c.fullname coursename 
+FROM prefix_logstore_standard_log l 
+INNER JOIN prefix_course c ON 
+l.courseid = c.id GROUP BY courseIdORDER BY hits DESC 
